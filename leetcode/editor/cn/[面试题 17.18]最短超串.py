a3bce9c3cd878cc95cec1e66e5f243053b28e680
shuxinyin <844040010@qq.com>
2022-02-09 23:36:22 +0800

    add leetcode 17-18

--- leetcode/editor/cn/[面试题 17.18]最短超串.py
+++ leetcode/editor/cn/[面试题 17.18]最短超串.py
@@ -28,6 +28,40 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def shortestSeq(self, big, small):
-        print()
+        import collections
+        minl, minr = -1, -1
+        l, r = 0, 0
+        min_len = len(big)
+        need = collections.Counter(small)
+        diff = len(small)
+
+        while r < len(big):
+            if big[r] in need:
+                need[big[r]] -= 1
+                if need[big[r]] >= 0:
+                    diff -= 1
+
+            while diff == 0:
+                if r - l < min_len:
+                    min_len = r - l
+                    minl, minr = l, r
+
+                if big[l] in need:  # 移动左边
+                    need[big[l]] += 1
+                    if need[big[l]] > 0:
+                        diff += 1
+                l += 1
+            r += 1
+
+        if minl == -1:
+            return []
+        return [minl, minr]
+
 
 # leetcode submit region end(Prohibit modification and deletion)
+
+if __name__ == '__main__':
+    big = [7, 5, 9, 0, 2, 1, 3, 5, 7, 9, 1, 1, 5, 8, 8, 9, 7]
+    small = [1, 5, 9]
+    S = Solution()
+    print(S.shortestSeq(big, small))
